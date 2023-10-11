73a6204e029ea970cb8e472a534d231b173e5e68
David Garcia <david.mago.obscuro@gmail.com>
2023-10-11 00:33:06 -0600

    pass rubocop

--- app/models/property_for_rent.rb
+++ app/models/property_for_rent.rb
@@ -1,10 +1,9 @@
 class PropertyForRent < ApplicationRecord
   belongs_to :property_id
   has_one :user_property, dependent: :destroy
-  
- # Validations
 
- validates :monthly_rent, :maintenance, numericality: { greater_than: 0 }, presence: true
- validates :pets_allowed, inclusion: [true, false]
+  # Validations
 
+  validates :monthly_rent, :maintenance, numericality: { greater_than: 0 }, presence: true
+  validates :pets_allowed, inclusion: [true, false]
 end
