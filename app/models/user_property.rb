3334a0b78d115e0339cfe10d1d2dd2975f79ff17
David Garcia <david.mago.obscuro@gmail.com>
2023-10-10 23:54:22 -0600

    db:reset

--- app/models/user_property.rb
+++ app/models/user_property.rb
@@ -1,5 +1,5 @@
 class UserProperty < ApplicationRecord
   belongs_to :user_id
-  belongs_to :property_for_rent_id , polymorphic: true
+  belongs_to :property_for_rent_id 
   belongs_to :property_for_sale_id 
 end
