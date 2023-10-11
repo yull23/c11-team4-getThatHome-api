c61d1880d8e107ae8c239aa34ccf36ca22d5a6dc
David Garcia <david.mago.obscuro@gmail.com>
2023-10-11 17:06:04 -0600

    add routes home/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  devise_for :users
-
-  root "users#show"
-  resources :roles
+  root to: "home#index"
 end
