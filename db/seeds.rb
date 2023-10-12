d721ae66638415fe0d5e340c445b619a36ea7b35
David Garcia Rendon <91983152+davegr2022@users.noreply.github.com>
2023-10-11 21:08:00 -0600

    Update seeds.rb
    Crea usuarios y sus roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Role.create(name: 'admin')
+Role.create(name: 'usuario')
+Role.create(name: 'editor')
+
+# Primero, obtén las instancias de los roles
+admin_role = Role.find_by(name: 'admin')
+user_role = Role.find_by(name: 'usuario')
+editor_role = Role.find_by(name: 'editor')
+
+# Crea usuarios con roles
+User.create(name: 'david', email: 'david@example.com', password: 'password1', password_confirmation: 'password1', role: admin_role)
+User.create(name: 'roxana', email: 'roxana@example.com', password: 'password2', password_confirmation: 'password2', role: user_role)
+User.create(name: 'yull', email: 'yull@example.com', password: 'password3', password_confirmation: 'password3', role: editor_role)
+
