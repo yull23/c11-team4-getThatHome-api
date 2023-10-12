d1028f60eb490b1658ac2d9213cf542f32a2e435
David Garcia <david.mago.obscuro@gmail.com>
2023-10-12 10:05:17 -0600

    change   config.sign_out_via = :get

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get "home/index"
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
+  get "home/index"
 
   root to: "home#index"
+
+
+
 end
