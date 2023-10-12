1696e4531c609bc733bb2bae15419dec33c85608
David Garcia <david.mago.obscuro@gmail.com>
2023-10-12 10:17:10 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  
+  devise_for :users
   get "home/index"
 
   root to: "home#index"
-
-
-
 end
