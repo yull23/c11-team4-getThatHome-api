7e076336aa8a26777d1a466499b00fb45a41db57
David Garcia <david.mago.obscuro@gmail.com>
2023-10-12 15:34:42 -0600

    fix user_pro

--- app/models/user_property.rb
+++ app/models/user_property.rb
@@ -1,5 +1,5 @@
 class UserProperty < ApplicationRecord
   belongs_to :user
-  belongs_to :property_for_rent_id
-  belongs_to :property_for_sale_id
+  belongs_to :propertyable polymorphic: true
+  
 end
