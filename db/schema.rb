99779ed0b1a576ccaecf30fea9264de66dcbdfeb
David Garcia <david.mago.obscuro@gmail.com>
2023-10-14 21:35:46 -0600

    add gem  bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_13_031749) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_030510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mi_usuarios", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_mi_usuarios_on_token", unique: true
+  end
 
   create_table "properties", force: :cascade do |t|
     t.bigint "property_type_id", null: false
