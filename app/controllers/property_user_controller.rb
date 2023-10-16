2c52edbc7cef0d99e9bd7e757a4c70227ed25320
David Garcia <david.mago.obscuro@gmail.com>
2023-10-15 21:39:35 -0600

    add property_user_controller

---
+++ app/controllers/property_user_controller.rb
@@ -0,0 +1,42 @@
+class PropertyUserController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_homeseeker_role, only: [:index, :create, :update]
+
+  def index
+    @saved_properties = SavedProperty.where(user: current_user)
+    render json: @saved_properties
+  end
+
+  def create
+    property = Property.find_by(id: params[:property_id])
+    @saved_property = SavedProperty.new(property: property, user: current_user)
+
+    if @saved_property.save
+      render json: @saved_property, status: :ok
+    else
+      render json: @saved_property.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @saved_property = SavedProperty.find_by(user: current_user, id: params[:id])
+
+    if @saved_property.update(saved_properties_params)
+      render status: :ok
+    else
+      render json: @saved_property.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def require_homeseeker_role
+    unless current_user.role_name == "Homeseeker"
+      render status: :unprocessable_entity, json: { error: "Acceso denegado. Se requiere el rol de Homeseeker." }
+    end
+  end
+
+  def saved_properties_params
+    params.permit(:favorite, :contacted)
+  end
+end
