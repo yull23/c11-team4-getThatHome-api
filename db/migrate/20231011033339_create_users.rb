7cb0f8094b0416b8fa0ce389a768c686aacec218
David Garcia <david.mago.obscuro@gmail.com>
2023-10-24 14:31:20 -0600

    fix create_user

--- db/migrate/20231011033339_create_users.rb
+++ db/migrate/20231011033339_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :name, presence: true
       t.string :phone
-      t.string :email, null: false
-      t.string :password_digest
+      t.string :email, presence: true, uniqueness: true
+      t.string :password_digest, presence: true, length: { minimum: 6 }
       t.string :token
       t.integer :role_id, null: false
       
